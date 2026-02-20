cc5e05dd80cd3450021365cbcba0983377e9390a
samlovelace <samlovelace12@gmail.com>
2026-02-19 21:11:49 -0500

    updates to the setup script

--- setup.sh
+++ setup.sh
@@ -1,3 +1,24 @@
+##########################################################################
+# Setup script to install ROS2 and system dependencies for the ABV stack
+##########################################################################
+
+# where to clone libs that need built from source 
+LIBS_DIR=~/libs
+
+# List of required system packages
+DEPENDENCIES=(
+    build-essential
+    cmake
+    curl
+    vim
+    tmux
+    libeigen3-dev
+    libyaml-cpp-dev
+    libsfml-dev
+    libboost-system-dev 
+    libboost-thread-dev
+)
+
 # Function to check and install a package 
 check_and_install() {
     PKG="$1"
@@ -57,25 +78,75 @@ install_from_source() {
     make install
 }
 
-LIBS_DIR=~/libs
+install_ros() {
+    echo "[ROS] Starting installation of ROS 2 Humble..."
 
-# List of required packages
-DEPENDENCIES=(
-    build-essential
-    cmake
-    git
-    curl
-    libeigen3-dev
-    libyaml-cpp-dev
-    libsfml-dev
-    libboost-system-dev 
-    libboost-thread-dev
-)
+    # --- Configure UTF-8 Locale ---
+    if ! locale | grep -q "UTF-8"; then
+        echo "[ROS] Configuring UTF-8 locale..."
+        apt install -y locales
+        locale-gen en_US en_US.UTF-8
+        update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+        export LANG=en_US.UTF-8
+    fi
 
+    # --- Set Timezone Non-Interactively ---
+    echo "[ROS] Setting timezone to America/New_York..."
+    ln -fs /usr/share/zoneinfo/America/New_York /etc/localtime
+    DEBIAN_FRONTEND=noninteractive apt install -y tzdata
+    dpkg-reconfigure -f noninteractive tzdata
+
+    # --- Add Required Tools ---
+    DEBIAN_FRONTEND=noninteractive apt install -y software-properties-common curl gnupg lsb-release
+
+    # --- Add Universe Repo (auto-confirm) ---
+    echo "[ROS] Adding universe repository..."
+    add-apt-repository -y universe
+
+    # --- Add ROS 2 GPG Key ---
+    echo "[ROS] Adding ROS 2 GPG key..."
+    curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key \
+        -o /usr/share/keyrings/ros-archive-keyring.gpg
+
+    # --- Add ROS 2 Repository ---
+    echo "[ROS] Adding ROS 2 repository..."
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] \
+          http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" \
+          > /etc/apt/sources.list.d/ros2.list
+
+    # --- Install ROS 2 Humble and Tools ---
+    echo "[ROS] Installing ROS 2 Humble and tools..."
+    apt update
+    DEBIAN_FRONTEND=noninteractive apt install -y ros-humble-desktop \
+        python3-colcon-common-extensions \
+        python3-rosdep \
+        python3-vcstool
+
+    # --- Source ROS Environment ---
+    echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
+    source /opt/ros/humble/setup.bash
+    echo "[✔] ROS 2 Humble installed and environment sourced."
+}
+
+###########################################################################
+## Script run logic 
+###########################################################################
+
+# get current dir so we can come back at the end to build the packages 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# make sure things are updated 
+apt update
+
+# treat ros2 install special 
+check_and_install "ros-humble-desktop" "install_ros"
+
+# install system deps 
 for pkg in "${DEPENDENCIES[@]}"; do
     check_and_install "$pkg"
 done
 
+# install deps from source
 install_from_source plog SergiusTheBest/plog 1.1.10 $LIBS_DIR
 install_from_source libmotioncapture samlovelace/libmotioncapture main "$LIBS_DIR" 
 
@@ -83,6 +154,8 @@ install_from_source libmotioncapture samlovelace/libmotioncapture main "$LIBS_DI
 clone_and_checkout JETGPIO Rubberazer/JETGPIO v1.2 "$LIBS_DIR"
 echo "orinagx" > hardware
 make && make install
-# end JETGPIO custom 
+# end JETGPIO custom  
 
-# TODO: ROS2 custom install function 
+# build the packages 
+cd $SCRIPT_DIR
+exec bash && colcon build
