f18140d6888231499474c8e3128e49732287e0ad
george-nehma <georgenehma98@gmail.com>
2026-04-10 13:29:39 -0400

    updates launch file to use sudo run for gnc

--- src/abv_bringup/launch/jetson.launch.py
+++ src/abv_bringup/launch/jetson.launch.py
@@ -6,21 +6,21 @@ from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
 
-    guidance = Node(
-        package='abv_guidance',
-        executable='abv_guidance',
-        output='screen',
-        emulate_tty=True)
+    guidance = ExecuteProcess(
+        name='abv_guidance',
+        cmd=['sudo', './scripts/run.sh', 'abv_guidance'],
+        additional_env={'ROS_DOMAIN_ID': os.environ.get('ROS_DOMAIN_ID', '0')},
+        output='screen')
 
-    navigation = Node(
-        package='abv_navigation',
-        executable='abv_navigation',
-        output='screen',
-        emulate_tty=True)
+    navigation = ExecuteProcess(
+        name='abv_navigation',
+        cmd=['sudo', './scripts/run.sh', 'abv_navigation'],
+        additional_env={'ROS_DOMAIN_ID': os.environ.get('ROS_DOMAIN_ID', '0')},
+        output='screen')
 
     controller = ExecuteProcess(
         name='abv_controller',
-        cmd=['sudo', './run.sh', 'abv_controller'],
+        cmd=['sudo', './scripts/run.sh', 'abv_controller'],
         additional_env={'ROS_DOMAIN_ID': os.environ.get('ROS_DOMAIN_ID', '0')},
         output='screen')
 
